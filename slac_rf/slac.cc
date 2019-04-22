f075f224035e6262593503e13f5ad7ba9713fb44
prchyr <prchyr@gmail.com>
2019-04-22 14:59:45 -0400

    changes

--- slac_rf/slac.cc
+++ slac_rf/slac.cc
@@ -223,11 +223,11 @@ int main(int argc,char** argv)
 
      Hep3Vector tx, rx;
       double theta=0, phi=0;
-      rx.setRThetaPhi(4000,115*degree, 0.);
+      rx.setRThetaPhi(5000,115*degree, 0.);
       radio->setRxPos(rx, 0);
-      rx.setRThetaPhi(4000, 125*degree, 0.);
+      rx.setRThetaPhi(5000, 125*degree, 0.);
       radio->setRxPos(rx, 1);
-      rx.setRThetaPhi(4000, 85*degree, 0.);
+      rx.setRThetaPhi(5000, 85*degree, 0.);
       radio->setRxPos(rx, 2);
       
       double rr=2.;
