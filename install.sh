777fe653a27b699033046b35330ad8ed75e2c3a3
prchyr <prchyr@gmail.com>
2019-07-10 01:31:59 -0400

    example

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 top_dir=$(pwd)
 export RS_SOURCE_DIR=$top_dir
 
+if [ -d "build" ]
+then
+    echo "deleting existing build directory, starting from scratch (it's ok)"
+    rm -R "build"
+fi
+
 # if [ -z "$1" ]
 # then
 #     mkdir -p slac_build &&
--- slac_rf/slac.cc
+++ slac_rf/slac.cc
@@ -275,7 +275,20 @@ int main(int argc,char** argv)
      radio->setTxPos(tx, 0);
      runManager->BeamOn(1);
    }
+   if(macro=="run6.mac"){
+     Hep3Vector offset(.6,0,2.);
+     Hep3Vector tx, rx;
+     double theta=0, phi=0;
+     rx.setRThetaPhi(4000,55*degree, 0.);
+     radio->setRxPos(rx+offset, 0);
+     rx.setRThetaPhi(6000, 75*degree, 0.);
+     radio->setRxPos(rx+offset, 1);
+     rx.setRThetaPhi(6000, 65*degree, 0.);
+     radio->setRxPos(rx+offset, 2);
 
+     runManager->BeamOn(10);
+   }
+   
    if(macro=="fanoutt576.mac"){
      Hep3Vector offset(.6,0,2.);//coordinate system is 0,0,0 at front of target. this offset is for the coordinate system used at ESA
      Hep3Vector tx, rx;
